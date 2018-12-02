ed452ec72ccda6fc815e12edcfb3a03213b62b32
Tomi Kalmi <tomi.kalmi@futurice.com>
2018-12-02 18:06:52 +0200

    Complete day 2

--- 2/checksum.py
+++ 2/checksum.py
@@ -19,6 +19,23 @@ def get_checksum(content):
     return two_count * three_count
 
 
+def get_common_chars_in_almost_matching_ids(content):
+    content_len = len(content)
+    str_len = len(content[0])
+    for i in range(content_len):
+        for j in range(content_len - i):
+            diff_count = 0
+            common_chars = ''
+            for c in range(str_len):
+                if content[i][c] is not content[j][c]:
+                    diff_count += 1
+                else:
+                    common_chars += content[i][c]
+            if diff_count is 1:
+                return common_chars
+
+
 content = read_content('input.txt')
 
 print('Checksum', get_checksum(content))
+print('Common chars', get_common_chars_in_almost_matching_ids(content))
