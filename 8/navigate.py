37ac5f2719e04621db00e3229958c61f7747675a
Tomi Kalmi <tomi.kalmi@futurice.com>
2018-12-08 16:42:21 +0200

    Solve day 8 pt. 2

--- 8/navigate.py
+++ 8/navigate.py
@@ -10,13 +10,22 @@ def get_metadata(content):
     metadata_count = content[1]
     metadata = []
     child_content = content[2:]
-    for _ in range(children_count):
-        (child_metadata, child_content) = get_metadata(child_content)
+    child_values = []
+    for i in range(children_count):
+        (child_metadata, child_content, child_value) = get_metadata(child_content)
+        child_values.append(child_value)
         metadata += child_metadata
-    metadata += child_content[:metadata_count]
-    return (metadata, child_content[metadata_count:])
+    own_metadata = child_content[:metadata_count]
+    metadata += own_metadata
+    if children_count is 0:
+        return (metadata, child_content[metadata_count:], sum(metadata))
+    value = 0
+    for i in own_metadata:
+        if i > 0 and i <= len(child_values):
+            value += child_values[i-1]
+    return (metadata, child_content[metadata_count:], value)
 
 
 content = get_content('input.txt')
-(metadata, rest) = get_metadata(content)
-print('Metadata sum', sum(metadata))
+(metadata, _, value) = get_metadata(content)
+print('Metadata sum', sum(metadata), 'root node value', value)
