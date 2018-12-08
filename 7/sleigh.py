7169b348b83e517f9b61d92a064cd8cc9a6a8a79
Tomi Kalmi <tomi.kalmi@futurice.com>
2018-12-08 16:42:52 +0200

    Solve day 7 pt. 2

--- 7/sleigh.py
+++ 7/sleigh.py
@@ -53,8 +53,47 @@ def traverse(tree):
                 tree.pop(key, None)
                 break
     print('Order in which parts should be assembled', ''.join(order))
+    return order
+
+
+class Elf:
+    def __init__(self):
+        self.working_on = None
+        self.time_to_finish = 0
+
+
+def traverse_time(tree):
+    time = -1
+    elves = [Elf(), Elf(), Elf(), Elf(), Elf()]
+    worked_nodes = []
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    while len(tree.keys()) > 0:
+        time += 1
+        for elf in elves:
+            elf.time_to_finish = max(0, elf.time_to_finish - 1)
+            if elf.time_to_finish is 0 and elf.working_on:
+                node = elf.working_on
+                for child in tree[node].children:
+                    tree[child].remove_parent(node)
+                worked_nodes.remove(node)
+                elf.working_on = None
+                tree.pop(node, None)
+
+        sorted_keys = sorted(tree.keys())
+        for key in sorted_keys:
+            if key not in worked_nodes and len(worked_nodes) < 5 and len(tree[key].parents) is 0:
+                worked_nodes.append(key)
+                for elf in elves:
+                    if not elf.working_on:
+                        elf.working_on = key
+                        elf.time_to_finish = 60 + alphabet.index(key) + 1
+                        break
+    print('Time taken to assemble', time)
+    return time
 
 
 content = get_content('input.txt')
 tree = get_tree(content)
 traverse(tree)
+tree = get_tree(content)
+traverse_time(tree)
